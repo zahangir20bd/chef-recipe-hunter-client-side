d1a25edf90b3dae4329e8d421462725b51caa19f
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-05-04 20:25:08 +0600

    Register Authentication and create user Done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,68 +1,80 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Form, Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Login = () => {
+  const { signIN } = useContext(AuthContext);
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+  };
+
   return (
-    <div>
-      <div>
-        <div className="hero min-h-screen bg-base-200 rounded-xl">
-          <div className="hero-content flex-col lg:flex-row-reverse lg:justify-between">
-            <div className="text-center lg:text-left ">
-              <h1 className="text-2xl font-bold mb-5 ">Continue With</h1>
-              <button className="btn btn-outline btn-primary w-full mb-2">
-                <FaGoogle className="me-1" />
-                <span>Login With Google</span>
-              </button>
-              <button className="btn btn-outline btn-primary w-full">
-                <FaGithub className="me-1" />
-                <span>Login With GitHub</span>
-              </button>
-              <p className="py-6"></p>
+    <div className="hero min-h-screen bg-base-200 rounded-xl">
+      <Form
+        onSubmit={handleLogin}
+        className="hero-content flex-col lg:flex-row-reverse lg:justify-between"
+      >
+        <div className="text-center lg:text-left ">
+          <h1 className="text-2xl font-bold mb-5 ">Continue With</h1>
+          <button className="btn btn-outline btn-primary w-full mb-2">
+            <FaGoogle className="me-1" />
+            <span>Login With Google</span>
+          </button>
+          <button className="btn btn-outline btn-primary w-full">
+            <FaGithub className="me-1" />
+            <span>Login With GitHub</span>
+          </button>
+          <p className="py-6"></p>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
             </div>
-            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-              <div className="card-body">
-                <h1 className="text-5xl font-bold">Login now!</h1>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Email</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="email"
-                    className="input input-bordered"
-                  />
-                </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Password</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="password"
-                    className="input input-bordered"
-                  />
-                  <label className="label">
-                    <a href="#" className="label-text-alt link link-hover">
-                      Forgot password?
-                    </a>
-                  </label>
-                </div>
-                <div className="form-control mt-6">
-                  <button className="btn btn-primary">Login</button>
-                </div>
-                <div>
-                  <span>New to here? Go to..</span>
-                  <Link className="text-primary" to="/register">
-                    {" "}
-                    Register
-                  </Link>
-                </div>
-              </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                name="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <Link className="label-text-alt link link-hover">
+                  Forgot password?
+                </Link>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
+            <div>
+              <span>New to here? Go to..</span>
+              <Link className="text-primary" to="/register">
+                Register
+              </Link>
             </div>
           </div>
         </div>
-      </div>
+      </Form>
     </div>
   );
 };
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,10 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Form, Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Register = () => {
+  const { createUser } = useContext(AuthContext);
+
+  const [error, setError] = useState("");
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.username.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const image = form.image.value;
+
+    if (
+      !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+        password
+      )
+    ) {
+      setError(
+        "Password must be 8 characters and at least one uppercase letter, one lowercase letter, one number and one special character"
+      );
+    }
+    if ((name, email, password)) {
+      createUser(email, password)
+        .then((result) => {
+          const createdUser = result.user;
+          console.log(createdUser);
+        })
+        .catch((error) => {
+          setError(error.message);
+        });
+    }
+  };
+
   return (
     <div className="hero min-h-screen bg-base-200 rounded-xl">
-      <div className="hero-content">
+      <Form onSubmit={handleRegister} className="hero-content">
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
             <h1 className="text-5xl font-bold">Register Here!</h1>
@@ -38,6 +72,7 @@ const Register = () => {
               </label>
               <input
                 type="password"
+                name="password"
                 placeholder="password"
                 className="input input-bordered"
                 required
@@ -45,32 +80,38 @@ const Register = () => {
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Image Link</span>
+                <span className="label-text">Image URL</span>
               </label>
               <input
                 type="text"
-                placeholder="Image link"
+                name="image"
+                placeholder="Image URL"
                 className="input input-bordered"
-                required
               />
             </div>
             <div className=" flex items-center gap-2">
-              <input type="checkbox" className="checkbox" />
+              <input
+                type="checkbox"
+                name="accept-condition"
+                className="checkbox"
+              />
               <span>Accept Terms & Condition</span>
             </div>
+            <p className="text-red-700">{error}</p>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Register</button>
+              <button type="submit" className="btn btn-primary">
+                Register
+              </button>
             </div>
             <div>
               <span>Already have an account? Go to..</span>
               <Link className="text-primary" to="/login">
-                {" "}
                 Login
               </Link>
             </div>
           </div>
         </div>
-      </div>
+      </Form>
     </div>
   );
 };
