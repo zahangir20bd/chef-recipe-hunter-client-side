a78c573211f18367fb49fda5eb63ce3e899c92a5
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-05-05 14:02:07 +0600

    404 Page implementation Complete and its functional

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,46 +1,44 @@
 import React from "react";
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "100vh",
-      }}
-    >
-      <Container maxWidth="md">
-        <Grid container spacing={2}>
-          <Grid xs={6}>
-            {/* <h1 className="text-[150px]">404</h1> */}
-            <Typography className="text-[300px]" variant="h1">
-              404
-            </Typography>
-            <Typography variant="h4">oops! Page not found.</Typography>
-            <p>
-              Oops! The Page you are looking for does not exist. It might have
-              been moved or deleted.
-            </p>
-            {/* <Typography variant="p">
-              Oops! The Page you are looking for does not exist. It might have
-              been moved or deleted.
-            </Typography> */}
-            <Button variant="contained">Back Home</Button>
+    <div className="px-6">
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
+        }}
+      >
+        <Container maxWidth="md">
+          <Grid container spacing={2}>
+            <Grid xs={6}>
+              <Typography variant="h1">404</Typography>
+              <Typography variant="h4">oops! Page not found.</Typography>
+              <p className="my-5">
+                Oops! The Page you are looking for does not exist. It might have
+                been moved or deleted.
+              </p>
+              <button className="btn btn-primary">
+                <Link to="/">Back to Home</Link>
+              </button>
+            </Grid>
+            <Grid xs={6}>
+              <img
+                src="https://cdn.pixabay.com/photo/2017/03/09/12/31/error-2129569__340.jpg"
+                alt=""
+                width={500}
+                height={250}
+              />
+            </Grid>
           </Grid>
-          <Grid xs={6}>
-            <img
-              src="https://cdn.pixabay.com/photo/2017/03/09/12/31/error-2129569__340.jpg"
-              alt=""
-              width={500}
-              height={250}
-            />
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
+        </Container>
+      </Box>
+    </div>
   );
 };
 
